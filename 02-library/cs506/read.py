99e9bed6965e49f5c722e279d10593761882f711
Jenny <68675135+warmsinging@users.noreply.github.com>
2021-10-07 16:21:03 -0400

    edit read.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -6,5 +6,4 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    df = pd.read_csv(csv_file_path)
-    return df
+    raise NotImplementedError()
