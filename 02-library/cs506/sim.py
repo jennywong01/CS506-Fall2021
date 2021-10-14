28596619c9f26c539d5ad17c7fc303bce3ffb850
Jenny <68675135+warmsinging@users.noreply.github.com>
2021-10-14 15:40:34 -0400

    modify tox

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,14 +1,17 @@
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
-        res += (x[i] - y[i])**2
-    return res**(1/2)
+        res += (x[i] - y[i]) ** 2
+    return res ** (1 / 2)
+
 
 def manhattan_dist(x, y):
     raise NotImplementedError()
 
+
 def jaccard_dist(x, y):
     raise NotImplementedError()
+
 
 def cosine_sim(x, y):
     raise NotImplementedError()
